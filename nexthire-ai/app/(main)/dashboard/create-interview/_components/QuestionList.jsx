a48e8cfa530e739f25e1ab693c561c49c859686c
prince902005@gmail.com <prince902005@gmail.com>
2025-04-21 16:25:47 +0530

    Implemented AI model and test

--- nexthire-ai/app/(main)/dashboard/create-interview/_components/QuestionList.jsx
+++ nexthire-ai/app/(main)/dashboard/create-interview/_components/QuestionList.jsx
@@ -1,48 +1,48 @@
 import axios from 'axios';
 import { Loader2Icon } from 'lucide-react';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { toast } from 'sonner';
 
 function QuestionList({ formData }) {
-
     const [loading, setLoading] = useState(true);
+    const [questionList, setQuestionList]= useState();
 
     useEffect(() => {
         if (formData) {
             GeneratQuestionList();
         }
-    }, [formData])
+    }, [formData]);
 
     const GeneratQuestionList = async () => {
         setLoading(true);
         try {
             const result = await axios.post('/api/ai-model', {
-                ...formData
-            })
-            console.log(result.data);
-            setLoading(false);
-        }
-        catch (e) {
-            toast('Server Error, please try again.')
-            setLoading(false);
-        }
-    }
+                ...formData,
+            });
+            console.log(result.data.content);
+            const Content = JSON.parse(result.data.content);
 
+            setQuestionList(Content);
+            setLoading(false);
+        } catch (e) {
+            toast('Server Error, please try again.');
+            setLoading(false);
+        }
+    };
 
     return (
         <div>
-            {loading && 
-            <div className='p-5 bg-blue-50 rounded-xl border border-gray-100 flex gap-5 items-center'>
-                <Loader2Icon className='animate-spin' />
-                <div>
-                    <h2>Generating Interview Questions</h2>
-                    <p>Our AI is crafting personalized questions based on your job details.</p>
+            {loading && (
+                <div className="p-5 bg-blue-50 rounded-xl border border-primary flex gap-5 items-center">
+                    <Loader2Icon className="animate-spin" />
+                    <div>
+                        <h2 className='font-medium'>Generating Interview Questions</h2>
+                        <p className='text-primary'>Our AI is crafting personalized questions based on your job details.</p>
+                    </div>
                 </div>
-
-            </div>
-            }
+            )}
         </div>
-    )
+    );
 }
 
-export default QuestionList
+export default QuestionList;
