c7efe4daf4190e21f340d6cecf00544116a0c17a
prince902005@gmail.com <prince902005@gmail.com>
2025-04-25 16:29:31 +0530

    Implementing vapi

--- nexthire-ai/app/interview/[interview_id]/page.jsx
+++ nexthire-ai/app/interview/[interview_id]/page.jsx
@@ -57,7 +57,7 @@ function Interview() {
         console.log(Interviews[0]);
         setInterviewInfo({
             userName: userName,
-            interviewData: Interview[0]
+            interviewData: Interviews[0]
         });
         router.push('/interview/' + interview_id + '/start')
         setLoading(false);
--- nexthire-ai/app/interview/[interview_id]/start/page.jsx
+++ nexthire-ai/app/interview/[interview_id]/start/page.jsx
@@ -2,14 +2,85 @@
 import { InterviewDataContext } from '@/context/InterviewDataContext'
 import { Mic, Phone, Timer, TimerIcon } from 'lucide-react'
 import Image from 'next/image'
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import Vapi from "@vapi-ai/web";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
 
 
 function StartInterview() {
   const { interviewInfo, setInterviewInfo } = useContext(InterviewDataContext)
   const vapi = new Vapi(process.env.NEXT_PUBIC_VAPI_PUBLIC_KEY);
 
+  useEffect(() => {
+    interviewInfo && startCall();
+  }, [interviewInfo])
+
+  const startCall = () => {
+    let questionList;
+    interviewInfo?.interviewData?.questionList.forEach((item, index) => (
+      questionList = item?.question + "," + questionList
+    ));
+
+
+    const assistantOptions = {
+      name: "AI Recruiter",
+      firstMessage: "Hi " + interviewInfo?.userName + ", how are you? Ready for your interview on " + interviewInfo?.interviewData.jobPosition,
+      transcriber: {
+        provider: "deepgram",
+        model: "nova-2",
+        language: "en-US",
+      },
+      voice: {
+        provider: "playht",
+        voiceId: "jennifer",
+      },
+      model: {
+        provider: "openrouter",
+        model: "nvidia/llama-3.3-nemotron-super-49b-v1:free",
+        messages: [
+          {
+            role: "system",
+            content: `
+  You are an AI voice assistant conducting interviews.
+Your job is to ask candidates provided interview questions, assess their responses.
+Begin the conversation with a friendly introduction, setting a relaxed yet professional tone. Example:
+"Hey there! Welcome to your `+ interviewInfo?.interviewData.jobPosition + ` interview. Let’s get started with a few questions!"
+Ask one question at a time and wait for the candidate’s response before proceeding. Keep the questions clear and concise. Below Are the questions ask one by one:
+Questions: `+ questionList + `
+If the candidate struggles, offer hints or rephrase the question without giving away the answer. Example:
+"Need a hint? Think about how React tracks component updates!"
+Provide brief, encouraging feedback after each answer. Example:
+"Nice! That’s a solid answer."
+"Hmm, not quite! Want to try again?"
+Keep the conversation natural and engaging—use casual phrases like "Alright, next up..." or "Let’s tackle a tricky one!"
+After 5-7 questions, wrap up the interview smoothly by summarizing their performance. Example:
+"That was great! You handled some tough questions well. Keep sharpening your skills!"
+End on a positive note:
+"Thanks for chatting! Hope to see you crushing projects soon!"
+Key Guidelines:
+✅ Be friendly, engaging, and witty 🎤
+✅ Keep responses short and natural, like a real conversation
+✅ Adapt based on the candidate’s confidence level
+✅ Ensure the interview remains focused on React
+`.trim(),
+          },
+        ],
+      },
+    };
+
+    vapi.start(assistantOptions)
+
+  }
 
 
   return (
@@ -41,8 +112,8 @@ function StartInterview() {
         </div>
       </div>
       <div className='flex items-center gap-5 justify-center mt-7'>
-        <Mic className='h-12 w-12 p-3 bg-gray-500 text-white rounded-full cursor-pointer'/>
-        <Phone className='h-12 w-12 p-3 bg-red-500 text-white rounded-full cursor-pointer'/>
+        <Mic className='h-12 w-12 p-3 bg-gray-500 text-white rounded-full cursor-pointer' />
+        <Phone className='h-12 w-12 p-3 bg-red-500 text-white rounded-full cursor-pointer' />
       </div>
       <h2 className='text-sm text-gray-400 text-center mt-4'>
         Interview is in Progress...
