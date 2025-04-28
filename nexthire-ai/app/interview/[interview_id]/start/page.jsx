71ec94d748e2460479aeb57a949015f4c95b12ea
prince902005@gmail.com <prince902005@gmail.com>
2025-04-28 23:24:28 +0530

    Update

--- nexthire-ai/app/interview/[interview_id]/start/page.jsx
+++ nexthire-ai/app/interview/[interview_id]/start/page.jsx
@@ -126,7 +126,7 @@ Key Guidelines:
 
         <div className='bg-white h-[400px] rounded-lg border flex flex-col gap-3 items-center justify-center'>
           <div className='relative'>
-          {!activeUser&& <span className='absolute inset-0 rounded-full bg-blue-500 opacity-75 animate-ping' />}
+          {activeUser&& <span className='absolute inset-0 rounded-full bg-blue-500 opacity-75 animate-ping' />}
           <h2 className='text-2xl bg-primary text-white p-3 rounded-full px-5'>
             {interviewInfo?.userName[0]}
           </h2>
