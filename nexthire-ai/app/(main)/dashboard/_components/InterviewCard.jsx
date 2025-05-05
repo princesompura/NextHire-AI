b090c3a49ea19f6075b63c279b67c0bf3358869d
prince902005@gmail.com <prince902005@gmail.com>
2025-05-06 00:47:10 +0530

    Implemented momentjs

--- nexthire-ai/app/(main)/dashboard/_components/InterviewCard.jsx
+++ nexthire-ai/app/(main)/dashboard/_components/InterviewCard.jsx
@@ -1,8 +1,15 @@
+import moment from 'moment'
 import React from 'react'
 
 function InterviewCard({ interview }) {
     return (
-        <div>InterviewCard</div>
+        <div className='p-5 bg-blue-50 rounded-lg border mt-5'>
+            <div className='flex items-center justify-between'>
+                <div className='h-[40px] w-[40px] bg-primary rounded-full '></div>
+                <h2>{moment(interview?.created_at).format('DD MMM yyy')}</h2>
+
+            </div>
+        </div>
     )
 }
 
