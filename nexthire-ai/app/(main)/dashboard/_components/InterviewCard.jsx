f1142b995eee25c9be2575942d203879af1d345a
prince902005@gmail.com <prince902005@gmail.com>
2025-05-24 15:06:05 +0530

    Fix Misconfigurations

--- nexthire-ai/app/(main)/dashboard/_components/InterviewCard.jsx
+++ nexthire-ai/app/(main)/dashboard/_components/InterviewCard.jsx
@@ -1,48 +1,59 @@
-import { Button } from '@/components/ui/button'
-import { ArrowRight, Copy, Send } from 'lucide-react'
-import moment from 'moment'
-import Link from 'next/link'
-import React from 'react'
-import { toast } from 'sonner'
+import { Button } from "@/components/ui/button";
+import { ArrowRight, Copy, Send } from "lucide-react";
+import moment from "moment";
+import Link from "next/link";
+import React from "react";
+import { toast } from "sonner";
 
 function InterviewCard({ interview, viewDetail = false }) {
+  const url = `${process.env.NEXT_PUBLIC_HOST_URL}/interview/${interview?.interview_id}`;
 
-    const url = process.env.NEXT_PUBLIC_HOST_URL + interview?.interview_id
+  const copyLink = () => {
+    navigator.clipboard.writeText(url);
+    toast("Copied");
+  };
 
-    const copyLink = () => {
+  const onSend = () => {
+    window.location.href =
+      "mailto:accounts@gmail.com?subject=Interview Link&body=Interview Link:" + url;
+  };
 
-        navigator.clipboard.writeText(url);
-        toast('Copied')
-    }
-
-    const onSend = () => {
-        window.location.href = "mailto:accounts@gmail.com?subject= Interview Link & body=Interview Link:" + url
-    }
-
-
-    return (
-        <div className='p-5 bg-blue-50 rounded-lg border mt-5'>
-            <div className='flex items-center justify-between'>
-                <div className='h-[40px] w-[40px] bg-primary rounded-full '></div>
-                <h2 className='text-sm'>{moment(interview?.created_at).format('DD MMM yyy')}</h2>
-
-            </div>
-            <h2 className='mt-3 font-bold text-lg'>{interview?.jobPosition}</h2>
-            <h2 className='mt-2 flex justify-between text-gray-500'>{interview?.interviewDuration}
-                <span className='text-green-700'>{interview['interview-feedback']?.length} Candidates</span>
-            </h2>
-            {!viewDetail ? <div className='flex gap-3 mt-2'>
-                <Button variant='outline' className=' bg-blue-200 hover:bg-blue-300 cursor-pointer' onClick={copyLink}> <Copy /> Copy Link</Button>
-                <Button className='bg-blue-200 hover:bg-blue-300 text-black cursor-pointer' onClick={onSend}> <Send className='text-black' /> Mail To</Button>
-            </div>
-                :
-                <Link href={'/Scheduled-interview/' + interview?.interview_id+"/details"}>
-                    <Button className='mt-5 w-full bg-blue-200 hover:bg-blue-300' variant='outline'>View Detail<ArrowRight /></Button>
-                </Link>
-            }
-
+  return (
+    <div className="p-5 bg-blue-50 rounded-lg border mt-5">
+      <div className="flex items-center justify-between">
+        <div className="h-[40px] w-[40px] bg-primary rounded-full"></div>
+        <h2 className="text-sm">{moment(interview?.created_at).format("DD MMM yyy")}</h2>
+      </div>
+      <h2 className="mt-3 font-bold text-lg">{interview?.jobPosition}</h2>
+      <h2 className="mt-2 flex justify-between text-gray-500">
+        {interview?.interviewDuration}
+        <span className="text-green-700">{interview["interview-feedback"]?.length} Candidates</span>
+      </h2>
+      {!viewDetail ? (
+        <div className="flex gap-3 mt-2">
+          <Button
+            variant="outline"
+            className="bg-blue-200 hover:bg-blue-300 cursor-pointer"
+            onClick={copyLink}
+          >
+            <Copy /> Copy Link
+          </Button>
+          <Button
+            className="bg-blue-200 hover:bg-blue-300 text-black cursor-pointer"
+            onClick={onSend}
+          >
+            <Send className="text-black" /> Mail To
+          </Button>
         </div>
-    )
+      ) : (
+        <Link href={`/Scheduled-interview/${interview?.interview_id}/details`}>
+          <Button className="mt-5 w-full bg-blue-200 hover:bg-blue-300" variant="outline">
+            View Detail <ArrowRight />
+          </Button>
+        </Link>
+      )}
+    </div>
+  );
 }
 
-export default InterviewCard
+export default InterviewCard;
--- nexthire-ai/app/(main)/dashboard/page.jsx
+++ nexthire-ai/app/(main)/dashboard/page.jsx
@@ -1,18 +1,30 @@
-import React from 'react'
-import WelcomeContainer from './_components/WelcomeContainer'
-import CreateOptions from './_components/CreateOptions'
-import LatestInterviewsList from './_components/LatestInterviewsList'
+"use client";
+import React, { useEffect } from "react";
+import WelcomeContainer from "./_components/WelcomeContainer";
+import CreateOptions from "./_components/CreateOptions";
+import LatestInterviewsList from "./_components/LatestInterviewsList";
+import { supabase } from "@/services/supabaseClient";
+import { useRouter } from "next/navigation";
 
 function Dashboard() {
-  return (
-    <div >
-      {/* <WelcomeContainer /> */}
-      <h2 className='my-3 font-bold text-2xl'>Dashboard</h2>
-      <CreateOptions />
+  const router = useRouter();
 
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      if (!session) {
+        router.push("/auth");
+      }
+    });
+  }, [router]);
+
+  return (
+    <div>
+      {/* <WelcomeContainer /> */}
+      <h2 className="my-3 font-bold text-2xl">Dashboard</h2>
+      <CreateOptions />
       <LatestInterviewsList />
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
